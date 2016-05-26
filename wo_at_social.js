f0116dc0a803870570f8d611fa3ab74e5f22d321
DevasenaInupakutika <devasena.prasad@gmail.com>
2016-05-26 06:17:28 +0100

    Dropdown button when the Oauth/ OpenIDConnect providers are web observatories

--- wo_at_social.js
+++ wo_at_social.js
@@ -1,3 +1,7 @@
 //Uses atSocial template from meteor useraccounts bootstrap in place of original atSocial.
 Template.woatSocial.replaces("atSocial");
 
+Template.registerHelper('equals', function(a, b) {
+
+   return a === b;
+});
