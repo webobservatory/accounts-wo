75464dfc861d46d06b16769681cfce2e6902022a
DevasenaInupakutika <devasena.prasad@gmail.com>
2016-05-26 17:01:55 +0100

    Populating template and list of web observatories from Javascript

--- wo_at_social.js
+++ wo_at_social.js
@@ -6,3 +6,54 @@ Template.registerHelper('equals', function(a, b) {
 
    return a === b;
 });
+
+Template.registerHelper('observatories', function(){
+        return ["Southampton Web Observatory",
+  "NUS Singapore NeXT Observatory",
+  "UNISA",
+  "COSMOS - Cardiff",
+  "RPI Web Observatory",
+  "Indiana University Truthy",
+  "SONIC - Northwestern",
+  "UI - Indonesia",
+  "IIIT-Bangalore",
+  "KAIST - Korea",
+  "Stanford - SNAP",
+  "The Koblenz Network Collection"]
+    
+});
+
+
+//Dropdown events
+Template.woatSocial.events({
+
+  "change #observatory-select": function (event, template) {
+        var observatory = $(event.currentTarget).val();
+        console.log("observatory : " + observatory);
+        // additional code to do what you want with the observatory
+    },
+
+  'click button': function(evt) {
+
+      evt.preventDefault();
+      evt.stopPropagation(); //Stop the full-page click handler from firing
+      Session.set('woatSocial', this.name);
+
+   },
+
+   'click .dropdown-menu li a': function(evt){
+
+      evt.preventDefault();
+      Session.set('woatSocial', null);
+   },
+
+   'change .dropdown-menu li a': function(event, t){
+
+
+   } 
+
+
+
+
+
+});
