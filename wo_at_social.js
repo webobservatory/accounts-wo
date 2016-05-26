8bca32638372c6b3e2f99ce8e17929f9fb938f85
DevasenaInupakutika <devasena.prasad@gmail.com>
2016-05-26 12:46:39 +0100

    Updated dropdown with list of active web observatories.

--- wo_at_social.js
+++ wo_at_social.js
@@ -1,6 +1,7 @@
 //Uses atSocial template from meteor useraccounts bootstrap in place of original atSocial.
 Template.woatSocial.replaces("atSocial");
 
+//Global helper to be used to compare OpenID connect providers so as to render the template accordingly.
 Template.registerHelper('equals', function(a, b) {
 
    return a === b;
