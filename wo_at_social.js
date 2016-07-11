02079245b3cfa4ff1a7d034272111f3c9cddd728
DevasenaInupakutika <devasena.prasad@gmail.com>
2016-07-11 12:55:11 +0100

    Functionalities added: Dropdown button for list of configured wo nodes and populating them dynamically

--- wo_at_social.js
+++ wo_at_social.js
@@ -7,49 +7,71 @@ Template.registerHelper('equals', function(a, b) {
    return a === b;
 });
 
-Template.registerHelper('observatories', function(){
-        return ["Southampton Web Observatory",
-  "NUS Singapore NeXT Observatory",
-  "UNISA",
-  "COSMOS - Cardiff",
-  "RPI Web Observatory",
-  "Indiana University Truthy",
-  "SONIC - Northwestern",
-  "UI - Indonesia",
-  "IIIT-Bangalore",
-  "KAIST - Korea",
-  "Stanford - SNAP",
-  "The Koblenz Network Collection"]
-    
+//Global helper to be used to check whether number of configured Web Observatory nodes for service WOOIDC are more than one.
+Template.registerHelper('morethanone', function(n, one) {
+
+  return n > one;
 });
 
+//Global helper to be used for finding the number of WO nodes configured.
+Template.registerHelper('configLength', function() {
+  
+  var schemaloginservice = ServiceConfiguration.configurations.findOne({ service: 'wooidc' });
 
-//Dropdown events
+  var config_length = schemaloginservice.config.length;
+  return config_length;
+
+});
+
+//Global helper to be used to compare service name and number of configured nodes for web observatory
+Template.registerHelper('wooidcconfig', function() {
+
+  var schemaloginservice = ServiceConfiguration.configurations.findOne({ service: 'wooidc' });
+ 
+  var config_length = schemaloginservice.config.length;
+
+  if ( (this._id === "wooidc")&&(config_length > 1) )
+     return true;
+  else
+     return false;
+});
+
+var nodeArray = [];
+
+//Global helper for populating WOOIDC dropdown menu with configured domains.
+Template.registerHelper('woNodes', function() {
+
+    var schemaloginservice = ServiceConfiguration.configurations.findOne({ service: 'wooidc' });
+    
+    var configArray = schemaloginservice.config;
+
+    return configArray.forEach(function (doc) {
+       console.log(doc.domain);
+       nodeArray.push(doc.domain);
+       console.log(nodeArray);
+       return nodeArray;
+    });
+
+}); 
+
+Template.registerHelper('node', function() {
+   
+    $('.dropdown-menu.pull-right').dropdown();
+
+});
+
+//Meteor woatSocial template events
 Template.woatSocial.events({
 
-  /*"change #observatory-select": function (event, template) {
-        var observatory = $(event.currentTarget).val();
-        console.log("observatory : " + observatory);
-        // additional code to do what you want with the observatory
-    },
-
-  'click button': function(evt) {
-
+  /* 'click .dropdown-menu li a': function(evt){
       evt.preventDefault();
-      evt.stopPropagation(); //Stop the full-page click handler from firing
-      Session.set('woatSocial', this.name);
-
-   },*/
-
-   'click .dropdown-menu li a': function(evt){
-
-      evt.preventDefault();
-      Session.set('woatSocial', null);
    },
 
-   'change .dropdown-menu li a': function(event, t){
-     //action to be performed when one of the web observatory openid connect provider is clicked.
-
-   } 
+   'click li#at-wooidc.suwo':function(event, t){
+      
+        event.preventDefault();
+        console.log($(event.target).text());
+    }*/
+          
 
 });
